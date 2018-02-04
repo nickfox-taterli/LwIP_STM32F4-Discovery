98420aa3cddb2be7aa53a41c073eac0528839c0d
TaterLi <admin@lijingquan.net>
2018-02-04 13:13:41 +0800

    测试mixed

--- Demo/Src/web_view.c
+++ Demo/Src/web_view.c
@@ -57,6 +57,20 @@ void web_view(void *arg)
             vTaskDelay(100);
 					
         }
+				
+				err = WebClient("http://ticks.applinzi.com/lwip/mixed.php?get_origin_price=5", postVar, &abuf);
+        if(abuf != NULL && err == HTTP_OK)
+        {
+					
+            root = cJSON_Parse((const char *)abuf);
+
+            origin_price = cJSON_GetObjectItem( root , "get_origin_price" );
+            real_price = cJSON_GetObjectItem( root , "real_price" );
+            vPortFree(abuf);
+						cJSON_Delete(root);
+            vTaskDelay(100);
+					
+        }
         vTaskDelay(1000);
     }
 }
